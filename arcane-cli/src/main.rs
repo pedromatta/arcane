46f58c545f200ee571c1842ccad94b97935005cf
pedromatta <contact@pedromatta.com>
2026-06-07 16:14:40 -0300

    feat: implement cli initial functions

--- arcane-cli/src/main.rs
+++ arcane-cli/src/main.rs
@@ -1,22 +1,99 @@
-use anyhow::{Context, Result};
+mod add;
+mod list;
+use anyhow::Result;
 use arcane_core::db::establish_connection;
+use arcane_core::error::ArcaneError;
+use clap::{Parser, Subcommand};
+use sqlx::SqlitePool;
 use std::env;
 
-pub async fn initialize_app(db_url: &str) -> Result<()> {
-    let _pool = establish_connection(db_url)
-        .await
-        .context("Failed to initialize databse pool and run embedded migrations");
+use crate::add::add_category;
+use crate::list::list_categories;
 
-    Ok(())
+pub async fn initialize_app(db_url: &str) -> Result<SqlitePool, ArcaneError> {
+    let pool = establish_connection(db_url).await?;
+
+    Ok(pool)
+}
+
+#[derive(Parser)]
+#[clap(
+    name = "Arcane",
+    version = "0.1",
+    author = "pedromatta",
+    about = "A Rust routine planner"
+)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Initialize the database and run embedded migrations
+    Init,
+    Log,
+    Categories {
+        #[command(subcommand)]
+        subcommand: CategoryCommands,
+    },
+}
+
+#[derive(Subcommand)]
+enum CategoryCommands {
+    /// Add a new category
+    Add {
+        /// Name of the category
+        #[arg(short, long)]
+        name: String,
+
+        /// Default duration for tasks in this category (in minutes)
+        #[arg(short, long, default_value_t = 25)]
+        default_minutes: u32,
+
+        /// Color of the category (hex code, eg. #FF5733)
+        #[arg(short, long, default_value = "#FFFFFF")]
+        color: String,
+    },
+    /// List all categories
+    List,
 }
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    dotenvy::dotenv().expect("Failed to load .env file");
+async fn main() {
+    let cli = Cli::parse();
 
+    dotenvy::dotenv().ok();
     let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set in .env file");
 
-    initialize_app(&db_url).await
+    let pool = initialize_app(&db_url).await.unwrap();
+
+    match &cli.command {
+        Some(Commands::Init) => {
+            dotenvy::dotenv().ok();
+            initialize_app(&db_url)
+                .await
+                .expect("Failed to initialize database");
+            println!("Database initialized successfully.");
+        }
+        Some(Commands::Log) => {
+            println!("Log command selected. (Not implemented yet)");
+        }
+        Some(Commands::Categories { subcommand }) => match subcommand {
+            CategoryCommands::Add {
+                name,
+                default_minutes,
+                color,
+            } => {
+                add_category(&pool, name, default_minutes, color).await;
+            }
+            CategoryCommands::List => {
+                list_categories(&pool).await;
+            }
+        },
+        None => {}
+    }
 }
 
 #[cfg(test)]
