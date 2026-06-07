9c56c5c301571cf207d8aa85fabff4a9c7f89c5b
pedromatta <contact@pedromatta.com>
2026-06-07 10:19:15 -0300

    feat: implement custom eror types

---
+++ arcane-core/src/error.rs
@@ -0,0 +1,32 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum ArcaneError {
+    #[error("Database error occurred: {0}")]
+    Database(#[from] sqlx::Error),
+
+    #[error("Database migration failed: {0}")]
+    Migration(#[from] sqlx::migrate::MigrateError),
+
+    #[error("Failed to parse configuration: {0}")]
+    Config(#[from] toml::de::Error),
+
+    #[error("Failed to parse structure date: {0}")]
+    Chrono(#[from] chrono::ParseError),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use sqlx::Error as SqlxError;
+
+    #[test]
+    fn formats_database_error_correctly() {
+        let sqlx_err = SqlxError::RowNotFound;
+        let app_err: ArcaneError = sqlx_err.into();
+        assert_eq!(
+            app_err.to_string(),
+            "Database error occurred: no rows returned by a query that expected to return at least one row"
+        );
+    }
+}
