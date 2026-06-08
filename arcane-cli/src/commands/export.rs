120f1a6d1806b10ee556cc3b79d0a95f89a8ade8
pedromatta <contact@pedromatta.com>
2026-06-08 20:53:33 -0300

    feat: implements the export command

--- arcane-cli/src/commands/export.rs
+++ arcane-cli/src/commands/export.rs
@@ -3,16 +3,14 @@ use sqlx::SqlitePool;
 
 pub async fn export_cmd(pool: &SqlitePool) {
     match export_manifest(pool).await {
-        Ok(manifest) => {
-            match toml::to_string(&manifest) {
-                Ok(toml_str) => {
-                    println!("{}", toml_str);
-                }
-                Err(e) => {
-                    eprintln!("Error serializing manifest to TOML: {}", e);
-                }
+        Ok(manifest) => match toml::to_string(&manifest) {
+            Ok(toml_str) => {
+                println!("{}", toml_str);
             }
-        }
+            Err(e) => {
+                eprintln!("Error serializing manifest to TOML: {}", e);
+            }
+        },
         Err(e) => {
             eprintln!("Error exporting manifest from database: {}", e);
         }
